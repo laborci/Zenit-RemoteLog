c492d6a63a29e7ef1670892e57aa7ba78bc98452
laborci <gergely@laborci.hu>
2020-02-03 12:41:48 +0100

    s

--- src/Module.php
+++ src/Module.php
@@ -9,6 +9,6 @@ class Module implements ModuleInterface {
 	public function load($config) {
 		\Zenit\Bundle\RemoteLog\Component\RemoteLog::Service()->registerErrorHandlers();
 		ServiceContainer::shared(SqlLogHookInterface::class)->service(SqlLogHook::class);
-		include __DIR__."/../Facade/dump.php";
+		include __DIR__."/Facade/dump.php";
 	}
 }
